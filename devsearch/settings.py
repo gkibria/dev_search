d88ede99465a2d3773981d166fd82ac1a8185cc8
Kibria <kibria@medicbd.com>
2024-01-20 13:34:38 +0100

    CSRF_TRUSTED_ORIGINS set to *

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -125,3 +125,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    '*'
+]
